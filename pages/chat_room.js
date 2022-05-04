4a20bb9f1be37ec6d9944b9327893c1dbbd8a754
coden-arfat <52942762+coden-arfat@users.noreply.github.com>
2022-05-04 20:52:18 +0600

    updated
    Signed-off-by: coden-arfat <52942762+coden-arfat@users.noreply.github.com>

---
+++ pages/chat_room.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const chat_room = () => {
+  return (
+    <div>chat_room</div>
+  )
+}
+
+export default chat_room
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import {Button} from 'react-bootstrap'
+export default function Home() {
+  return (
+
+    <>
+    <h1>
+      hello world
+      <Button>hello </Button>
+    </h1>
+    </>
+
+  )
+}
